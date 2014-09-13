990522703585d5b1a0252df5a18b9f1cd54b0475
SimplyFaisal <d.f.animashaun@gmail.com>
2014-09-13 04:40:11 -0400

    started landing and FB login

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var request = require("request");
+var passport = require("./fb_login");
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -13,6 +14,18 @@ app.get('/api',function(req,res){
 	});
 });
 
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
