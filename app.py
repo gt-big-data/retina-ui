106eb14dfdea90224fc7f22481abae9f529d3dc9
SimplyFaisal <d.f.animashaun@gmail.com>
2014-10-01 01:07:22 -0400

    reimplemented stub

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import webapp2
+import logging
+
+with open("stub.json","r") as f:
+    stub = f.read()
+    
+    
 
 class MainPage(webapp2.RequestHandler):
     
@@ -6,6 +12,13 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class Stub(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/javascript'
+        self.response.write(str(stub))
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/api/hello', MainPage),
+    ('/api/stub',Stub),
 ], debug=True)
