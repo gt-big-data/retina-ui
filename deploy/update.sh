8c5e58d7b2ae39270c4317b563c8c88e805d97f0
Nathan Cheek <myself@nathancheek.com>
2014-10-29 19:51:49 -0400

    Add update.sh

---
+++ deploy/update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Update webserver by pulling from Github
+while true; do
+  read -p "Do you want to stop the node server?" yn
+  case $yn in
+    [Yy]* ) stopBool="true"; break;;
+    [Nn]* ) stopBool="false"; break;;
+    * ) echo "Type y or n";;
+  esac
+done
+echo ""
+if [ "$stopBool" = "true" ]; then
+  #stop the node server
+  result=`ps -aux | grep node`
+  echo "List of node processes"
+  echo $result
+  echo "Enter process ID to kill"
+  read killID
+  echo "You entered $killID"
+  kill $killID
+  echo ""
+fi
+echo "Pulling from Github"
+git pull
+echo "Restarting node server"
+nohup node web &
+wait ${!}
+echo "Done updating"
+echo ""
