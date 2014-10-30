24c9ede1cb918b8a7d6e88a80cf0b94de1e4d9b5
Nathan Cheek <myself@nathancheek.com>
2014-10-29 20:00:33 -0400

    Update update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -13,16 +13,17 @@ if [ "$stopBool" = "true" ]; then
   #stop the node server
   result=`ps -aux | grep node`
   echo "List of node processes"
+  echo "USER       PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND"
   echo "$result"
   echo "Enter process ID to kill"
   read killID
-  echo "You entered $killID"
   kill $killID
+  echo "Killed $killID"
   echo ""
 fi
 echo "Pulling from Github"
 git pull
 echo "Restarting node server"
 nohup node web &
-wait ${!}
+sleep 1
 echo "Done updating"
