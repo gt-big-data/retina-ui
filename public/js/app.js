556141a73db814f5d73f5ddd5b42c58c7534a275
nathancheek <myself@nathancheek.com>
2014-11-10 19:32:12 -0500

    Updates to front page news navbar

--- public/js/app.js
+++ public/js/app.js
@@ -78,6 +78,52 @@ app.controller("hn",["$scope","$http",function($scope,$http){
 		$scope.show = function() {
 			scope.news.show = true;
 		}
-
 	});
 }]);
+
+app.controller("sizeNewsNav",["$scope",function sizeNewsNav($scope) {
+		if(fontsLoaded !== true) {//Wait till fonts are loaded
+			console.log("Fonts aren't loaded yet")
+			setTimeout(sizeNewsNav, 50);//Recursion!
+			return;
+		}
+		else {//Fonts are loaded, proceed!
+			console.log("Resizing news-nav-items")
+			var navWidth = 0;
+			$("#news-nav-items li").each(function() {
+				navWidth = navWidth + $(this).width() + 30;//30 pixels for margin-right; have to do it this way because of WebKit bug
+			})
+			$("#news-nav-items").css({"width":navWidth+30,"visibility":"visible"}).hide().fadeIn("slow");//30 pixels for padding-left
+
+			$(document).ready(function() {//Runs again when document has finished loading (I did this to fix a bug on some mobile devices but its still occuring)
+				console.log("Resizing news-nav-items (page loaded)")
+				var navWidth = 0;
+				$("#news-nav-items li").each(function() {
+					navWidth = navWidth + $(this).width() + 30;//30 pixels for margin-right; have to do it this way because of WebKit bug
+				})
+				$("#news-nav-items").css("width",navWidth+30);//30 pixels for padding-left
+			});
+
+			$(window).resize(function() {//Runs again when window is resized
+				console.log("Resizing news-nav-items (page resized)")
+				var navWidth = 0;
+				$("#news-nav-items li").each(function() {
+					navWidth = navWidth + $(this).width() + 30;//30 pixels for margin-right; have to do it this way because of WebKit bug
+				})
+				$("#news-nav-items").css("width",navWidth+30);//30 pixels for padding-left
+			})
+		}
+}]);
+
+app.directive('scrollToId', function() {
+  return {
+    scope: {
+      scrollTo: "@"
+    },
+    link: function(scope, $elm,attr) {
+      $elm.on('click', function() {
+        $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top - 54}, "slow");
+      });
+    }
+  }
+})
