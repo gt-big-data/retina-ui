6667dfc496a94ffa7cad23d298c34914b01143ad
Philippe Laban <tingofurro@googlemail.com>
2015-03-20 00:43:50 -0400

    Some kind of working gui

--- public/squares/JS/ui.js
+++ public/squares/JS/ui.js
@@ -1,8 +1,7 @@
 var screenW, screenH;
-var tileW = 300, tileH = 170;
+var tileW = 370, tileH = 200;
 var rows, cols;
 var config = [];
-var fillPattern = [];
 var loadInterval;
 function initialize() {
 	screenW = $(document).width();
@@ -17,18 +16,18 @@ function initialize() {
 }
 function loadConfig() {
 	if(!$.cookie('squareConfig')) {
-		config[0] = {width: 1, height: 1, r: 0, c: 0, category: 'Politics'};
-		config[1] = {width: 1, height: 1, r: 1, c: 0, category: 'World'};
-		config[2] = {width: 1, height: 1, r: 0, c: 1, category: 'Technology'};
-		saveConfig(); checkFill();
+		config[0] = {width: 1, height: 1, category: 'Politics'};
+		config[1] = {width: 1, height: 1, category: 'World'};
+		config[2] = {width: 1, height: 1, category: 'Technology'};
+		saveConfig();
 	}
 	else {
 		config = []; bug = false;
 		var strConfig = $.cookie('squareConfig').split('[]');
 		for (var i = 0; i < strConfig.length; i++) {
 			conf = strConfig[i].split('|');
-			if(conf.length >= 5) {
-				config.push({width: +conf[0], height: +conf[1], r: +conf[2], c: +conf[3], category: conf[4]});
+			if(conf.length >= 3) {
+				config.push({width: +conf[0], height: +conf[1], category: conf[2]});
 			}
 			else {
 				bug = true;
@@ -36,27 +35,30 @@ function loadConfig() {
 		}
 		if(bug == true) { // format was incorrect... redoing it
 			$.removeCookie('squareConfig');	loadConfig();	
-		} else checkFill();
+		}
 	}
 }
 function saveConfig() {
 	var buildStr = []; 
 	for (var i = 0; i < config.length; i++) {
 		conf = config[i];
-		buildStr.push(conf.width+'|'+conf.height+'|'+conf.r+'|'+conf.c+'|'+conf.category);
+		buildStr.push(conf.width+'|'+conf.height+'|'+conf.category);
 	}
 	$.cookie('squareConfig', buildStr.join('[]'));
 }
 function buildUI() {
 	for (var i = 0; i < config.length; i++) {
 		conf = config[i];
-		var newTile = '<div id="tile'+i+'" class="tile" style="top: '+(conf.r*tileH)+'px; left: '+(conf.c*tileW)+'px; width: '+(tileW*conf.width)+'px; height: '+(tileH*conf.height)+'px;">';
+		var newTile = '<div id="tile'+i+'" class="tile" style="width: '+(tileW*conf.width)+'px; height: '+(tileH*conf.height)+'px;">';
 		newTile += '<div class="tileBack">&nbsp;</div>';
 		newTile += '<div class="bottomGradient">&nbsp;</div>';
 		newTile += '<div class="tileTitle">'+conf.category+'</div>';
 		newTile += '<img class="tileCat" src="images/categoryIcons/'+conf.category.toLowerCase()+'.png" alt="'+conf.category+'" title="'+conf.category+'" />';
 		newTile += '</div>';
 		$('#content').append(newTile);
+	}
+	if(rows*cols > config.length) {
+		addNewTile();
 	}
 }
 function reloadInput() {
@@ -75,21 +77,8 @@ function reloadTile(index, article) {
 	$('#tile'+index+' .tileBack').css({"background-image": "url("+article.img+")"});
 
 }
-function checkFill() {
-	resetFill();
-	for (var i = 0; i < config.length; i++) {
-		conf = config[i];
-		for (var w = 0;  w< conf.width; w++) {
-			for (var h = 0;  h < conf.height; h++) fillPattern[(w+conf.r)][(h+conf.c)] = 1;
-		}
-	}
-	console.log(fillPattern);
-}
-function resetFill() {
-	fillPattern = [];
-	for (var i = 0; i < rows; i++) {
-	fillPattern[i] = [];
-	for (var j = 0; j < cols; j++) fillPattern[i][j] = 0;
-	}
+function addNewTile() {
+	newTile = '<div id="newTile" style="width: '+tileW+'px; height: '+tileH+'px;">Add a new category</div>';
+	$('#content').append(newTile);
 }
 initialize();
