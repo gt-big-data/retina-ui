23168d24d7156ec9b4f5a29d4aa1d4ff3748e5d5
Philippe Laban <plaban3@gatech.edu>
2015-04-19 12:05:59 -0400

    GraphTopico, dynamic reload of graph

--- public/graphTopico/loadGraph.js
+++ public/graphTopico/loadGraph.js
@@ -1,13 +1,102 @@
-var myTime = 0;
+var graph;
+function myGraph() {
+	this.addNode = function (id, group) {
+		nodes.push({"id": id, "group": group, "tagged": true});
+	};
+	this.update = function() {
+		update();
+	}
+	this.removeNodeObj = function (n) {
+		document.title='Yolo'+n.id;
+		var i = 0;
+		while (i < links.length) {
+			if ((links[i]['source'] == n) || (links[i]['target'] == n)) {
+				links.splice(i, 1);
+			}
+			else i++;
+		}
+		nodes.splice(this.findNodeIndex(n.id), 1);
+	};
+	this.removeLink = function (source, target) {
+		for (var i = 0; i < links.length; i++) {
+			if (links[i].source.id == source && links[i].target.id == target) {
+				links.splice(i, 1);
+				break;
+			}
+		}
+	};
+	this.tagNodes = function() {
+		for(i in nodes) nodes[i].tagged = false;
+	};
+	this.tagLinks = function() {
+		for(i in links) links[i].tagged = false;
+	};
+	this.removeExtraNodes = function() {
+		for(var i = 1; i < nodes.length; i ++) {
+			n = nodes[i];
+			if(!n.tagged) {this.removeNodeObj(n);}
+		}
+	};
+	this.changeColor = function(newNode, oldNode) {
+		newN = this.findNode(newNode);
+		oldN = this.findNode(oldNode);
+		newN.group = oldN.group;
+	};
+	this.addLink = function (source, target, value) {
+		links.push({"source": this.findNode(source), "target": this.findNode(target), "value": value});
+	};
 
-var width = $(document).width(), height = $(document).height();
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+	this.findNode = function (id) {
+		for (var i in nodes) {if (nodes[i]["id"] === id) return nodes[i];};
+		return -1;
+	};
+
+	this.findNodeIndex = function (id) {
+		for (var i = 0; i < nodes.length; i++) {
+			if (nodes[i].id == id) {return i;}
+		};
+		return -1;
+	};
+
+	var w = $(document).width(), h = $(document).height();
+
+	var color = d3.scale.category10();
+
+	var vis = d3.select("body").append("svg:svg").attr("width", w).attr("height", h).attr("id", "svg").attr("pointer-events", "all").attr("viewBox", "0 0 " + w + " " + h).attr("perserveAspectRatio", "xMinYMid").append('svg:g');
+
+	var force = d3.layout.force();
+	var nodes = force.nodes(), links = force.links();
+
+	var update = function () {
+		var link = vis.selectAll("line").data(links, function (d) {return d.source.id + "-" + d.target.id;});
+
+		link.enter().append("line").attr("id", function (d) { return d.source.id + "-" + d.target.id;}).attr("stroke-width", function (d) { return d.value / 10;}).attr("class", "link");
+		
+		link.exit().remove();
+
+		var node = vis.selectAll("g.node").data(nodes, function (d) {return d.id;});
+
+		var nodeEnter = node.enter().append("g").attr("class", "node");
+		nodeEnter.append("svg:circle").attr("r", 3).attr("id", function (d) {return "Node;" + d.id;}).attr("class", "nodeStrokeClass").attr("fill", function(d) { return color(d.group); });
+
+		node.exit().remove();
+
+		force.on("tick", function () {
+			node.attr("transform", function (d) {return "translate(" + d.x + "," + d.y + ")";});
+			link.attr("x1", function (d) {return d.source.x;}).attr("y1", function (d) {return d.source.y;}).attr("x2", function (d) {return d.target.x;}).attr("y2", function (d) {return d.target.y;});
+		});
+		force.charge(-40).linkDistance(10).linkDistance( function(d) { return d.value } ).size([w, h]).start(); // Restart the force layout.
+	};
+	// Make it all go
+	update();
+}
+
 function startDay() {
-	myTime = new Date().getTime();
-	myTime = myTime;
+	myTime = new Date().getTime(); myTime = myTime;
+	graph = new myGraph("#svgdiv");
+
 	reloadGraph();
 }
-startDay();
 function changeDay(which) {
 	myTime += which*86400000;
 	reloadGraph();
@@ -17,32 +106,41 @@ function reloadGraph() {
 	var fullDate = dateObj.getFullYear()+'-'+(((dateObj.getMonth()+1)<10)?'0':'')+(dateObj.getMonth()+1)+'-'+((dateObj.getDate()<10)?'0':'')+dateObj.getDate();
 	$('#whatDay').html(fullDate);
 	$.getJSON("/api/topics/filter?day="+fullDate, function( data ) {
-		if(data[0]) {
-			graph = data[0].graph;
-			svg.remove();
-			svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+		if(data[0]) {mergeGraphs(data[0].graph);}
+	});
+}
 
+function mergeGraphs(graphData) {
+	graph.tagNodes();
+	graph.tagLinks();
+	newNodes = [];
+	for(nod in graphData.nodes) {
+		myNode = graphData.nodes[nod];
+		if((n = graph.findNode(myNode.id)) != -1) {n.tagged = true;}
+		else {graph.addNode(myNode.id, myNode.group); newNodes.push(myNode.id);}
+	}
+	graph.removeExtraNodes();
 
-			var color = d3.scale.category20();
-
-			var force = d3.layout.force().charge(-40).linkDistance(10).size([width, height]);
-
-			force.nodes(graph.nodes).links(graph.edges).start();
-
-			var link = svg.selectAll(".link").data(graph.edges).enter().append("line").attr("class", "link").style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-			var node = svg.selectAll(".node").data(graph.nodes).enter().append("circle").attr("class", "node").attr("r", 3).style("fill", function(d) { return color(d.group); }).call(force.drag);
-
-			node.append("title").text(function(d) { return d.name; });
-
-			force.on("tick", function() {
-				link.attr("x1", function(d) { return d.source.x; }).attr("y1", function(d) { return d.source.y; }).attr("x2", function(d) { return d.target.x; }).attr("y2", function(d) { return d.target.y; });
-				node.attr("cx", function(d) { return d.x; }).attr("cy", function(d) { return d.y; });
-			});
-			
+	for(i in graphData.edges) {
+		myEdge = graphData.edges[i];
+		var f1 = $.inArray(myEdge.source, newNodes)!=-1;
+		var f2 = $.inArray(myEdge.target, newNodes)!=-1;
+		if(f1 || f2) {
+			graph.addLink(myEdge.source, myEdge.target, myEdge.value);
+			if(!(f1 && f2)) {
+				if(f1) {newNode = myEdge.source; oldNode = myEdge.target;}
+				else   {newNode = myEdge.target; oldNode = myEdge.source;}
+				graph.changeColor(newNode, oldNode);
+			}
 		}
-		else {
-			svg.remove();
-		}
-	});	
+	}
+	graph.update();
+	keepNodesOnTop();
+}
+startDay();
+function keepNodesOnTop() {
+	$(".nodeStrokeClass").each(function( index ) {
+		var gnode = this.parentNode;
+		gnode.parentNode.appendChild(gnode);
+	});
 }
