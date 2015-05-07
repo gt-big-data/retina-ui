a90bf0109aa7111c8cb6a41cf08a231ebe5377dc
Tally <Tali@ipsec-143-215-27-114.vpn.gatech.edu>
2015-05-08 01:50:46 +0200

    finished landing, login, and registration pages

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -1,10 +1,23 @@
 var retina = angular.module('retina', ['ui.router']);
 
-retina.config(function($stateProvider, $urlRouterProvider) {
+retina.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
 	$urlRouterProvider.otherwise('home');
+
 	$stateProvider.state('home', {
 		url: '/home',
 		templateUrl: '../../views/home.html'
+	});
+
+	$stateProvider.state('login', {
+		url: '/login',
+		templateUrl: '../../views/login.html'
 	})
+
+	$stateProvider.state('register', {
+		url: '/register',
+		templateUrl: '../../views/register.html'
+	})
+
+	// $locationProvider.html5Mode(true);
 });
