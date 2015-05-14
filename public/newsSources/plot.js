058f0a777420963c386cba2e0f750c589bf79783
Philippe Laban <plaban3@gatech.edu>
2015-05-14 18:52:20 +0200

    Aha

--- public/newsSources/plot.js
+++ public/newsSources/plot.js
@@ -20,24 +20,22 @@ function plotGraph(counts) {
       .attr("height", height + margin.top + margin.bottom)
     .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  d3.tsv("data.tsv", function(error, data) {
-    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-    var firstSource = counts[0].values;
-    x.domain([firstSource[firstSource.length-1].date, firstSource[0].date]);
+  var firstSource = counts[0].values;
+  x.domain([firstSource[firstSource.length-1].date, firstSource[0].date]);
 
-    y.domain([0,200]);
+  y.domain([0,200]);
 
-    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+  svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
 
-    svg.append("g").attr("class", "y axis").call(yAxis).append("text")
-        .attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Articles Posted");
+  svg.append("g").attr("class", "y axis").call(yAxis).append("text")
+      .attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Articles Posted");
 
-    var city = svg.selectAll(".city").data(counts).enter().append("g").attr("class", "city");
+  var city = svg.selectAll(".city").data(counts).enter().append("g").attr("class", "city");
 
-    city.append("path").attr("class", "line").attr("d", function(d) { return line(d.values); }).style("stroke", function(d) { return color(d.name); });
+  city.append("path").attr("class", "line").attr("d", function(d) { return line(d.values); }).style("stroke", function(d) { return color(d.name); });
 
-    city.append("text").datum(function(d) { return {name: d.name, value: d.values[0]}; })
-        .attr("transform", function(d) {return "translate(" + x(d.value.date) + "," + y(d.value.articles) + ")"; }).attr("x", 3).attr("dy", ".35em").attr('class', 'labels')
-        .text(function(d) { return d.name; }).attr('fill', function(d) { return color(d.name); });
-  });
+  city.append("text").datum(function(d) { return {name: d.name, value: d.values[0]}; })
+      .attr("transform", function(d) {return "translate(" + x(d.value.date) + "," + y(d.value.articles) + ")"; }).attr("x", 3).attr("dy", ".35em").attr('class', 'labels')
+      .text(function(d) { return d.name; }).attr('fill', function(d) { return color(d.name); });
+
 }
