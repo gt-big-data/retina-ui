caa55c515e16cda3249a01b9b5dcf5291da89b87
Sam Marder <sam.marder@useriq.com>
2016-01-25 12:30:18 -0500

    Changed port to 3000 for production server.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 var app = require('./application.js');
-var server = app.listen(5000, function() {
+var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
